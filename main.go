5031b9e81c8824e79df9f614ac29d1a7c2ca68de
kelleydv <kelleydv@gmail.com>
2017-05-31 22:02:36 -0700

    manage frontend dependencies with yarn

--- main.go
+++ main.go
@@ -7,8 +7,7 @@ import (
 
 func main() {
 	AppRouter = NewRouter() // declared in handlers.go
-	AppRouter.
-		PathPrefix("/static/").
-		Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+	AppRouter.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+	AppRouter.PathPrefix("/vendor/static/").Handler(http.StripPrefix("/vendor/static/", http.FileServer(http.Dir("./node_modules"))))
 	log.Fatal(http.ListenAndServe(":8080", AppRouter))
 }
