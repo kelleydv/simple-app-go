85af970a76bb9b5a78761b3e1792f68e3ee4cb6a
kelleydv <kelleydv@gmail.com>
2017-05-31 17:26:27 -0700

    cleanup

--- static/app.js
+++ static/app.js
@@ -1,42 +1,42 @@
 $(function () {
-	$("body").fadeIn();
+	$('body').fadeIn();
 
 	// Submit forms
-	$(".modal button[type='submit']").click(function () {
-		var form = $(this).parents(".modal").find("form");
-		var data = getFormData(form);
+	$('.modal form').submit(function (event) {
+		event.preventDefault();
+		var data = getFormData($(this));
 		$.ajax({
-           type: "POST",
-           url: form.attr("action"),
-		   contentType: "application/json; charset=utf-8",
-		   dataType: "json",
-           data: JSON.stringify(data),
-           success: function (resp) {
-			   $("#nav-unauthed").hide();
-			   $("#username").text(resp.data.username);
-			   $("#nav-authed").fadeIn();
-               console.log(resp);
-           },
-		   error: function (resp) {
-			   console.log(resp);
-		   }
-       });
+			type: 'POST',
+			url: $(this).attr('action'),
+			contentType: 'application/json; charset=utf-8',
+			dataType: 'json',
+			data: JSON.stringify(data),
+			success: function (resp) {
+				$('#nav-unauthed').hide();
+				$('#username').text(resp.data.username);
+				$('#nav-authed').fadeIn();
+			},
+			error: function (resp) {
+				console.log(resp);
+			}
+		});
+		$(this).parents('.modal').modal('toggle')
 	})
 
 	// Dismiss authenticated view
-	$("#nav-authed button").click(function () {
-		$("#nav-authed").hide();
-		$("#username").text("");
-		$("#nav-unauthed").fadeIn();
+	$('#nav-authed button').click(function () {
+		$('#nav-authed').hide();
+		$('#username').text('');
+		$('#nav-unauthed').fadeIn();
 	})
 })
 
 function getFormData($form){
-    var dataArray = $form.serializeArray();
-    var dataObject = {};
+	var dataArray = $form.serializeArray();
+	var dataObject = {};
 
-    $.map(dataArray, function(n, i){
-        dataObject[n['name']] = n['value'];
-    });
-    return dataObject;
+	$.map(dataArray, function(n, i){
+		dataObject[n.name] = n.value;
+	});
+	return dataObject;
 }
